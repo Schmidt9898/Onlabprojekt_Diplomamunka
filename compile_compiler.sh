ffbeb5faed300ff4594ed146f90b5d1ebd0ddf39
Schmidt9898 <schmidtlacus@gmail.com>
2022-11-10 12:50:26 +0100

    fixed bug in start and makefile

--- compile_compiler.sh
+++ compile_compiler.sh
@@ -15,7 +15,7 @@ make -j$(nproc)
 cd ..
 mkdir build2
 cd build2
-CC=../build/bin/clang CXX=../build/bin/clang++ cmake ../llvm/ -DCMAKE_BUILD_TYPE=Release -DCLANG_OPENMP_NVPTX_DEFAULT_ARCH=sm_70,sm_80 -DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=70,80 -DLLVM_TARGETS_TO_BUILD="X86;NVPTX" -DLLVM_ENABLE_PROJECTS="clang;openmp" -DCMAKE_INSTALL_PREFIX=/home/shared/software/clang/15.3
+CC=../build/bin/clang CXX=../build/bin/clang++ cmake ../llvm/ -DCMAKE_BUILD_TYPE=Release -DCLANG_OPENMP_NVPTX_DEFAULT_ARCH=sm_70,sm_80 -DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=70,80 -DLLVM_TARGETS_TO_BUILD="X86;NVPTX" -DLLVM_ENABLE_PROJECTS="clang;openmp" -DCMAKE_INSTALL_PREFIX=/home/shared/software/clang/15.3-centos8
 make -j$(nproc)
 make install
 
